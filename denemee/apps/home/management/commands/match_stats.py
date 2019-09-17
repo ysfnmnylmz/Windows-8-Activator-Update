e748140f6679ca65da88c591e2d3497f65e7078a
Yusuf Numan YILMAZ <ysfnmnylmz@gmail.com>
2019-09-17 12:44:52 +0300

    t

--- denemee/apps/home/management/commands/match_stats.py
+++ denemee/apps/home/management/commands/match_stats.py
@@ -9,6 +9,7 @@ from denemee.apps.result.models import Matches
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         teams = []
+        #test
         time = strftime("%m/%d/%Y", gmtime())
         #time = "9/16/2019"
         main_url = "http://www.betistuta.de"
