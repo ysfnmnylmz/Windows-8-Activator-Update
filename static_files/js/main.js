273dcc340e28bf76547d0cd9ff7b1cfd26c745e8
Yusuf Numan YILMAZ <ysfnmnylmz@gmail.com>
2019-09-19 19:03:50 +0300

    fix

--- static_files/js/main.js
+++ static_files/js/main.js
@@ -1,6 +1,59 @@
-$('.card').click(function () {
-    $(".card").addClass('wow animated fadeOutUpBig');
-    $(".card2").css("opacity", "1");
-    $(".card2").css("z-index", "1");
-    $(".card").css("opacity", "0.04");
-});
+var Flipper = (function() {
+  var card = $('.card');
+  var flipper = card.find('.card__flipper');
+  var win = $(window);
+
+  var flip = function() {
+    var thisCard = $(this);
+    var thisFlipper = thisCard.find('.card__flipper');
+    var offset = thisCard.offset();
+    var xc = win.width() / 2;
+    var yc = win.height() / 2;
+    var docScroll = $(document).scrollTop();
+    var cardW = thisCard.outerWidth() / 2;
+    var cardH = thisCard.height() / 2;
+
+    var transX = xc - offset.left - cardW;
+    var transY = docScroll + yc - offset.top - cardH;
+//     if (offset.top > card.height()) transY = docScroll - offset.top + cardH;
+    if (win.width() <= 700) transY = 0;
+
+    if (card.hasClass('active')) unflip();
+    thisCard.css('background', 'none')
+    thisCard.css({'z-index': '3'}).addClass('active');
+
+    thisFlipper.css({
+      'transform': 'translate3d(' + transX + 'px,' + transY + 'px, 0) rotateY(180deg) scale(1)',
+      '-webkit-transform': 'translate3d(' + transX + 'px,' + transY + 'px, 0) rotateY(180deg) scale(1)',
+      '-ms-transform': 'translate3d(' + transX + 'px,' + transY + 'px, 0) rotateY(180deg) scale(1)'
+    }).addClass('active');
+
+    return false;
+  };
+
+  var unflip = function(e) {
+    card.css({'z-index': '1'}).removeClass('active');
+    card.css('background','#db6623')
+    flipper.css({
+      'transform': 'none',
+      '-webkit-transform': 'none',
+      '-ms-transform': 'none'
+    }).removeClass('active');
+  };
+
+  var bindActions = function() {
+    card.on('click', flip);
+    win.on('click', unflip);
+  }
+
+  var init = function() {
+    bindActions();
+  };
+
+  return {
+    init: init
+  };
+
+}());
+
+Flipper.init();
