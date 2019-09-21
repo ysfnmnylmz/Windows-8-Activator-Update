83dd1867ec8698e786da15818703d7950cb67ba3
Yusuf Numan YILMAZ <ysfnmnylmz@gmail.com>
2019-09-21 17:43:38 +0300

    k

--- denemee/apps/result/urls.py
+++ denemee/apps/result/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
-from denemee.apps.result.views import yesterday
+from denemee.apps.result.views import yesterday, SearchMatch
 
 app_name = 'result'
 
 urlpatterns = [
     path('', yesterday, name='yesterday'),
+path('search/', SearchMatch.as_view(), name='search_result'),
 ]
--- denemee/apps/result/views.py
+++ denemee/apps/result/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from denemee.apps.result.models import Matches
 from time import strftime
+from django.db.models import Q
+from django.views.generic import TemplateView, ListView
 from datetime import datetime, timedelta
 
 
@@ -10,3 +12,16 @@ def yesterday(request):
         'y_matches': y_matches
     }
     return render(request, 'yesterday.html', payload)
+
+
+class SearchMatch(ListView):
+    model = Matches
+    template_name = 'match_search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        for term in query.split():
+            object_list = Matches.objects.filter(
+                Q(home_team__icontains=term) | Q(away_score__contains=term)
+            )
+            return object_list
